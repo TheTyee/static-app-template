95233c7b26a1389f54f96024297e2425a856591b
Phillip Smith <ps@phillipadsmith.com>
2014-08-20 12:14:40 -0400

    Back out changes so that the timeseries is just an example, not an implementation

--- ui/js/timeseries.js
+++ ui/js/timeseries.js
@@ -1,10 +1,15 @@
+////////////////////////////////////////////
+// Slide - Chart - Timeseries
+// This is just an example to start from
+// when developing new timeseries charts
+// in this project
+///////////////////////////////////////////
 var csv_data;
 
 // Find the window dimensions
 var margins = {top: 10, right: 80, bottom: 60, left: 60};
-var width = parseInt(d3.select("#d3-chart").style("width")) 
+var width = parseInt(d3.select("#d3-chart").style("width"));
 height = parseInt(d3.select("#d3-chart").style("height"));
-// console.log( width, height );
 
 // Setup container & chart dimensions
 var container_dimensions = {width: width, height: height},
@@ -15,54 +20,45 @@ chart_dimensions = {
 
 // Setup SVG
 var chart = d3.select("#d3-chart")
-.append("svg")
-.attr("width", container_dimensions.width)
-.attr("height", container_dimensions.height)
-.append("g")
-.attr("transform", "translate(" + margins.left + "," + margins.top + ")")
-.attr("id","chart");
+    .append("svg")
+        .attr("width", container_dimensions.width)
+        .attr("height", container_dimensions.height)
+        .append("g")
+        .attr("transform", "translate(" + margins.left + "," + margins.top + ")")
+        .attr("id","chart");
 
 // Variables for scales & axis
-var time_scale, percent_scale, pop_scale;
-var time_axis, count_axis, pop_axis;
+var time_scale, percent_scale;
+var time_axis, count_axis;
 
 // Draw function
 function draw(data) {
 	// Make the data avaialble to the console
 	csv_data = data;
-
-	// Just population data
-	var pop_data = data.map( function(d) { return [ d.year, d.pop ] } );
-
 	// Get the extend of the data	
-	var years = d3.extent(data, function(d) { return d.year });
-	var ghcs = d3.extent(data, function(d) { return d.ghcs });
-	var pops = d3.extent(data, function(d) { return d.pop });
+        var years = d3.extent(data, function(d) { return d.year });
+        var ghcs = d3.extent(data, function(d) { return d.ghcs });
+
 	// Muck with dates
 	var low_year = new Date ( years[0] );
 	var high_year = new Date ( years[1] );
 	low_year = low_year.setDate( low_year.getDate() - 365 );
 	high_year = high_year.setDate( high_year.getDate() + 365 );
+
 	// Setup the scales
 	time_scale = d3.time.scale()
-	.range([0,chart_dimensions.width])
-	.domain([low_year,high_year]);
+            .range([0,chart_dimensions.width])
+            .domain([low_year,high_year]);
 	percent_scale = d3.scale.linear()
-	.range([chart_dimensions.height, 0])
-	.domain([ghcs[0]-50,ghcs[1]+10]);
-	pop_scale = d3.scale.linear()
-	.range([chart_dimensions.height, 0])
-	.domain([pops[0]-1,pops[1]+1]);
+            .range([chart_dimensions.height, 0])
+            .domain([ghcs[0]-50,ghcs[1]+10]);
 
 	// Setup axis
 	time_axis = d3.svg.axis()
-	.scale(time_scale);		
+            .scale(time_scale);
 	count_axis = d3.svg.axis()
-	.scale(percent_scale)
-	.orient("left");
-	pop_axis = d3.svg.axis()
-	.scale(pop_scale)
-	.orient("right");
+            .scale(percent_scale)
+            .orient("left");
 
 	// Add the time axis
 	chart.append("g")
@@ -73,11 +69,6 @@ function draw(data) {
 	chart.append("g")
 	.attr("class", "y axis")
 	.call(count_axis);
-	// Add the pop axis
-	chart.append("g")
-	.attr("class", "p axis")
-	.attr("transform", "translate(" + chart_dimensions.width + " , 0)")
-	.call(pop_axis);
 
 	// Axis labels
 	// Add the y label
@@ -99,67 +90,31 @@ function draw(data) {
 
 	// Create lines for the values
 	var line = d3.svg.line()
-	.x(function(d){return time_scale(d.year)})
-	.y(function(d){return percent_scale(d.ghcs)})
-	.interpolate("linear");	
-	var k_line = d3.svg.line()
-	.x(function(d){return time_scale(d.year)})
-	.y(function(d){return percent_scale(d.kyoto)})
-	.interpolate("linear");	
-	var p_line = d3.svg.line()
-	.x(function(d){return time_scale(d.year)})
-	.y(function(d){return pop_scale(d.pop)})
-	.interpolate("linear");	
+            .x(function(d){return time_scale(d.year)})
+            .y(function(d){return percent_scale(d.ghcs)})
+            .interpolate("linear");	
 
 	// Add a group for the lines
-	// TODO Need to add various groups for lines,
-	// then add a more generalized selector for transitions
-	// and mouse overs
-
 	var g = d3.select("#chart")
 	.append("g")
 	g.append("path")
 	.attr("class", "line")
 	.attr("id", "ghcs")
 	.attr("d", line(data));
-	g.append("path")
-	.attr("class", "line")
-	.attr("id", "kyoto")
-	.attr("d", k_line(data));
-	g.append("path")
-	.attr("class", "line")
-	.attr("id", "ghcs")
-	.attr("d", p_line(data));
 
-	// This is not DRY!
-	// GCHs
-	var g_ghcs = d3.select("#chart")
+	var g = d3.select("#chart")
 	.append("g");
 	// Add points to line
-	g_ghcs.selectAll("circle")
-	.data(data)
-	.enter()
-	.append("circle")
-	.attr("cx", function(d) {return time_scale( d.year )})
-	.attr("cy", function(d) {return percent_scale( d.ghcs )})
-	.attr("r",0)
-	.attr("class", "circle");
-
-	// Population
-	var g_pop = d3.select("#chart")
-	.append("g");
-	// Add points to line
-	g_pop.selectAll("circle")
-	.data(data)
-	.enter()
-	.append("circle")
-	.attr("cx", function(d) {return time_scale( d.year )})
-	.attr("cy", function(d) {return pop_scale( d.pop )})
-	.attr("r",0)
-	.attr("class", "circle");
+	g.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+                .attr("cx", function(d) {return time_scale( d.year )})
+                .attr("cy", function(d) {return percent_scale( d.ghcs )})
+                .attr("r",0)
+                .attr("class", "circle");
 
 	// Add mouse overs & transitions (GHCs)
-
 	chart.selectAll("circle")
 	.on("mouseover", function(d){
 		d3.select(this)
@@ -178,7 +133,7 @@ function draw(data) {
 	.attr("r", 5);
 
 
-	g_ghcs.selectAll("circle")
+	g.selectAll("circle")
 	.on("mouseover.tooltip", function(d){
 		d3.select("text#ghcs").remove();
 		d3.select("#chart")
@@ -189,7 +144,7 @@ function draw(data) {
 		.attr("id", "ghcs");
 
 	});
-	g_ghcs.selectAll("circle")
+	g.selectAll("circle")
 	.on("mouseout.tooltip", function(d){
 		d3.select("text#ghcs")
 		.transition()
@@ -198,14 +153,13 @@ function draw(data) {
 		.remove();
 	});
 
-
 	// Responsive function
 	// When the window changes size, re-calcuate the scales & re-draw
 	function resize() {
 		/* Find the new window dimensions */
 		var width = parseInt(d3.select("#d3-chart").style("width")) - margins.left, 
 		height = parseInt(d3.select("#d3-chart").style("height")) - margins.bottom;
-		console.log( width, height );
+
 		/* Update the range of the scale with new width/height */
 		time_scale.range([0, width]).nice(d3.time.year);
 		percent_scale.range([height, 0]).nice();
@@ -214,7 +168,7 @@ function draw(data) {
 		chart.select('.x.axis')
 		.attr("transform", "translate(0," + height + ")")
 		.call(time_axis);
-		//
+
 		chart.select('.y.axis')
 		.call(count_axis);
 
@@ -227,10 +181,13 @@ function draw(data) {
 		.attr("cx", function(d) {return time_scale( d.year )})
 		.attr("cy", function(d) {return percent_scale( d.ghcs )});
 
+                time_axis.ticks(Math.max(height/50, 2));
+                count_axis.ticks(Math.max(width/50, 2));
+                
+
 	}
 	d3.select(window).on('resize', resize);
-
-}
+};
 
 
 d3.csv("/data/data.csv", function(d) {
@@ -241,4 +198,3 @@ d3.csv("/data/data.csv", function(d) {
 		kyoto: +d.Kyoto
 	};
 }, draw );
-
